a5006a4be20a68a09e9f7b73f5499038c584039b
YuraMyasnikov <yuriy.myasnikov.88@mail.ru>
2021-12-14 18:05:00 +0500

    new fix

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -1,14 +1,14 @@
 import React from "react";
 import './title.css'
 
-const Title = ({all, done, rest}) => {
+const Title = ({totalCount,doneCount, todoCount}) => {
     return(
         <div className='row'>
             <h1 className='col'>Zagolovok</h1>
             <span className='info d-flex'>
-                <span className='status text-primary'>&nbsp; всего: {all}</span>,
-                <span className='status text-success'>&nbsp; готовы: {done}</span>,
-                <span className=' status text-info'>&nbsp; осталось сделать {rest}.</span>
+                <span className='status text-primary'>&nbsp; всего: {totalCount}</span>,
+                <span className='status text-success'>&nbsp; готовы: {doneCount}</span>,
+                <span className=' status text-info'>&nbsp; осталось сделать {todoCount}.</span>
             </span>
         </div>
     )
